de8c00dd9e25f55775a06bc15287a7d5e3225076
fayiz120 <mfaisep@gmail.com>
2023-08-21 15:52:35 +0530

    Added sound

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const dice = document.getElementById("dice");
 
     rollButton.addEventListener("click", function () {
+
+        // Generate audio
+        var diceAudio = new Audio('Sounds/diceSound.mp3');
+        diceAudio.play();
+
         // Generate a random number between 1 and 6 (simulate a dice roll)
         const randomNumber = Math.floor(Math.random() * 6) + 1;
-        var img1 = "images/dice"+randomNumber+".png";
+        var img1 = "images/dice" + randomNumber + ".png";
 
         // Update the dice display
         dice.textContent = randomNumber;
-        document.querySelector("img").setAttribute("src",img1);
+        document.querySelector("img").setAttribute("src", img1);
 
     });
 });
